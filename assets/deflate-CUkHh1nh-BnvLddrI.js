66c556e8732c7a38eac3b7b175a77246cc76b0f0
hugo <hdamitzian@gmail.com>
2024-12-05 18:51:03 +0100

    Updates

--- assets/deflate-CUkHh1nh-BnvLddrI.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./pako.esm-CPws4d4z-DQSVoCko.js";import{g as o}from"./basedecoder-DvumDe3Y-gdlnpIMU.js";class f extends o{decodeBlock(r){return e(new Uint8Array(r)).buffer}}export{f as default};
--- assets/raw-Dt1HMqYc-CCYStf-G.js
+++
@@ -1,1 +0,0 @@
-import{g as d}from"./basedecoder-DvumDe3Y-gdlnpIMU.js";class t extends d{decodeBlock(e){return e}}export{t as default};
