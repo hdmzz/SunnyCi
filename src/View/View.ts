04f9bbeb9f934496e713f05786d3452e272ad878
Ana <ana.andric@esker.Com>
2024-12-23 17:56:57 +0100

    Slider UX apple

--- src/View/View.ts
+++ src/View/View.ts
@@ -127,19 +127,19 @@ class	View extends THREE.EventDispatcher {
 
 	// private updateHourSlider(){
 
-			// this.sunPath.updateHour()).listen()
+	// 		this.sunPath.updateHour()).listen()
 	// }
 
-	// private	createGUI() {
-	// 	const	gui = new GUI();
-	// 	const	sunLightFolder = gui.addFolder( 'SunLight' );
+	private	createGUI() {
+		const	gui = new GUI();
+		const	sunLightFolder = gui.addFolder( 'SunLight' );
 
-	// 	// sunLightFolder.add( sunParams, 'minute', 0, 60, 1 ).onChange(() => this.sunPath.updateHour()).listen();
-	// 	// sunLightFolder.add( sunParams, 'hour', 0, 24, 1 ).onChange(() => this.sunPath.updateHour()).listen();
-	// 	// sunLightFolder.add( sunParams, 'day', 1, 30, 1 ).onChange(() => this.sunPath.updateMonth()).listen();
-	// 	// sunLightFolder.add( sunParams, 'month', 1, 12, 1 ).onChange(() => this.sunPath.updateMonth()).listen();
-	// 	sunLightFolder.add( sunParams, 'minute', 0, 120, 1 ).onChange(() => this.sunPath.updateHour()).listen();
-	// };
+		sunLightFolder.add( sunParams, 'minute', 0, 60, 1 ).onChange(() => this.sunPath.updateHour()).listen();
+		sunLightFolder.add( sunParams, 'hour', 0, 24, 1 ).onChange(() => this.sunPath.updateHour()).listen();
+		sunLightFolder.add( sunParams, 'day', 1, 30, 1 ).onChange(() => this.sunPath.updateMonth()).listen();
+		sunLightFolder.add( sunParams, 'month', 1, 12, 1 ).onChange(() => this.sunPath.updateMonth()).listen();
+		// sunLightFolder.add( sunParams, 'minute', 0, 120, 1 ).onChange(() => this.sunPath.updateHour()).listen();
+	};
 
 	private	initSun() {
 		this.sunPath = new SunPath( sunParams, this.sunLight, this.center );
